9a73c93f67b96d3f583dc6e0a51d44f43f27d3b8
Cody Schafer <dev@codyps.com>
2020-03-17 23:38:17 -0400

    client credit dumping too

---
+++ src/bin/remule-db.rs
@@ -0,0 +1,56 @@
+use clap::{Arg, App, SubCommand, crate_name, crate_version, crate_authors};
+use std::error::Error;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new(crate_name!())
+        .author(crate_authors!())
+        .version(crate_version!())
+        .subcommand(SubCommand::with_name("known2")
+            .arg(Arg::with_name("known2-dat")
+                .required(true)
+                .index(1)))
+        .subcommand(SubCommand::with_name("clients")
+            .arg(Arg::with_name("clients-met")
+                .required(true)
+                .index(1)))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("known2", Some(submatches)) => {
+            for f in submatches.values_of_os("known2-dat").unwrap() {
+                match std::fs::File::open(f) {
+                    Ok(mut h) => {
+                        let mut b = Vec::default();
+                        h.read_to_end(&mut b)?;
+                        println!("{:?}", remule::known2::parse(&mut b));
+                    },
+                    Err(e) => {
+                        eprintln!("error: could not open {:?}: {:?}", f, e);
+                    }
+                }
+            }
+        },
+        ("clients", Some(submatches)) => {
+            for f in submatches.values_of_os("clients-met").unwrap() {
+                match std::fs::File::open(f) {
+                    Ok(mut h) => {
+                        let mut b = Vec::default();
+                        h.read_to_end(&mut b)?;
+                        println!("{:?}", remule::clientcredit::parse(&mut b));
+                    },
+                    Err(e) => {
+                        eprintln!("error: could not open {:?}: {:?}", f, e);
+                    }
+                }
+            }
+
+        },
+        (subname, _) => {
+            Err(format!("unknown subcommand {:?}", subname))?;
+        }
+    }
+
+
+    Ok(())
+}
--- src/clientcredit.rs
+++ src/clientcredit.rs
@@ -13,6 +13,8 @@ const CREDITFILE_VERSION_29: u8 = 0x11;
 
 use std::convert::TryInto;
 use std::error::Error;
+use plain::Plain;
+use fmt_extra::Hs;
 
 /*
 struct CreditStruct {
@@ -35,11 +37,11 @@ struct CreditStruct {
 const MAX_PUBKEYSIZE: usize = 80;
 
 // emule marks these with pragma pack(1), check if we need any explicit padding
-#[repr(C)]
+#[repr(C, packed)]
  struct CreditData29a {
     key: [u8;16],
-    upload_lo: u32,
-    download_lo: u32,
+    uploaded_lo: u32,
+    downloaded_lo: u32,
 
     last_seen: u32, // 32-bit seconds since-epoch
 
@@ -48,23 +50,50 @@ const MAX_PUBKEYSIZE: usize = 80;
     reserved: u16,
 }
 
-#[repr(C)]
+unsafe impl Plain for CreditData29a {}
+
+#[repr(C, packed)]
 struct CreditData {
     base: CreditData29a,
     // these only exist in version 0x12, not 0x11
     key_size: u8,
-    aby_secure_ident: [u8;MAX_PUBKEYSIZE]
+    secure_ident: [u8;MAX_PUBKEYSIZE]
 }
 
+unsafe impl Plain for CreditData {}
 
+pub fn split_from<P: Plain>(buf: &[u8]) -> (&P, &[u8]) {
+    let sz = std::mem::size_of::<P>();
+    let (i, rem) = buf.split_at(sz);
+    (P::from_bytes(&i).unwrap(), rem)
+}
 
-#[derive(Default, Debug)]
-struct ClientCredit {
-    key: [u8;16],
-    downloaded: u64,
-    uploaded: u64,
-    last_seen: std::time::SystemTime,
-    secure_ident: Vec<u8>,
+#[derive(Debug)]
+pub struct ClientCredit {
+    pub key: Hs<[u8;16]>,
+    pub downloaded: u64,
+    pub uploaded: u64,
+    pub last_seen: std::time::SystemTime,
+    pub secure_ident: Hs<Vec<u8>>,
+}
+
+impl ClientCredit {
+    fn from_data(data: &CreditData) -> Self {
+        let mut s = Self::from_data_29(&data.base);
+        s.secure_ident.extend(&data.secure_ident[..(data.key_size as usize)]);
+        s
+    }
+
+    fn from_data_29(data: &CreditData29a) -> Self {
+        Self {
+            key: Hs(data.key),
+            downloaded: (u32::from_le(data.downloaded_hi) as u64) << 32 | (u32::from_le(data.downloaded_lo) as u64),
+            uploaded: (u32::from_le(data.uploaded_hi) as u64) << 32 | (u32::from_le(data.uploaded_lo) as u64),
+            // XXX: Y2038 BUG
+            last_seen: std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(u32::from_le(data.last_seen) as u64),
+            secure_ident: Hs(Vec::new()),
+        }
+    }
 }
 
 pub fn parse(inp: &[u8]) -> Result<Vec<ClientCredit>, Box<dyn Error>> {
@@ -73,36 +102,56 @@ pub fn parse(inp: &[u8]) -> Result<Vec<ClientCredit>, Box<dyn Error>> {
     }
 
     let version = inp[0];
-    match version {
-        CREDITFILE_VERSION | CREDITFILE_VERSION_29 => {},
+    let entry_size = match version {
+        CREDITFILE_VERSION => {
+            std::mem::size_of::<CreditData>()
+        },
+        CREDITFILE_VERSION_29 => {
+            std::mem::size_of::<CreditData29a>()
+        }
         _ => {
             return Err(format!("unhandled version {}", version))?;
         }
-    }
+    };
 
-    let rem = &inp[1..];
+    let mut rem = &inp[1..];
 
     if rem.len() < 4 {
         return Err(format!("missing count, need 4 bytes, have {}", rem.len()))?;
     }
 
-    let count = u32::from_le_buf(rem[..4].try_into().unwrap());
-    // TODO: check length
+    let count = u32::from_le_bytes(rem[..4].try_into().unwrap()) as usize;
+    rem = &rem[4..];
 
-    let n = count * std::mem::size_of<CreditData29a>();
+    let n = count * entry_size;
+    if rem.len() < n {
+        return Err(format!("not enough space, need {} bytes ({} entries {} bytes each), have {}",
+            n, count, entry_size, rem.len()))?;
+    }
 
+    if rem.len() != n {
+        return Err(format!("spare bytes {}, ({} entries, {} bytes each, {} buf bytes, {} bytes needed",
+            rem.len() - n, count, entry_size, rem.len(), n))?;
+    }
+
+    let mut r = Vec::with_capacity(count);
     for _ in 0..count {
-        let mut cc = ClientCredit::default();
-        match version {
-            CREDITFILE_VERSION_29|CREDITFILE_VERSION => {
-                // load common fields
-            
+        let c = match version {
+            CREDITFILE_VERSION_29 => {                
+                let (cf, rr) = split_from::<CreditData29a>(rem);
+                rem = rr;
+                ClientCredit::from_data_29(cf)
+            },
+            CREDITFILE_VERSION => {
+                let (cf, rr) = split_from::<CreditData>(rem);
+                rem = rr;
+                ClientCredit::from_data(cf)
             },
             _ => panic!()
-        }
+        };
 
-        if version == CREDITFILE_VERSION {
-            // load extra fields from version 0x12
-        }
+        r.push(c);
     }
+
+    Ok(r)
 }
