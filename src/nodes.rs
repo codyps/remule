075f12d9f7300eba38e5182015adc206554e033b
Cody Schafer <dev@codyps.com>
2020-03-19 01:06:26 -0400

    more nodes.dat work

--- src/nodes.rs
+++ src/nodes.rs
@@ -7,14 +7,32 @@ use std::convert::TryInto;
 
 #[derive(Debug)]
 pub struct Contact {
+    // bootstrap/version 0/1 fields
     pub uid: u128,
     pub ip: u32,
     pub udp_port: u16,
     pub tcp_port: u16,
+
+    // version >= 1
     pub contact_version: u8,
+    // version 0
+    pub by_type: Option<u8>,
+    // version >= 2
+    // (key, ip)
+    pub kad_udp_key: Option<(u32, u32)>,
+    // version >= 2
+    pub verified: Option<bool>,    
 }
 
-pub fn parse_version_1(inp: &[u8]) -> Result<Vec<Contact>, Box<dyn Error>> {
+#[derive(Debug)]
+pub struct Nodes {
+    pub version: u32,
+    pub is_bootstrap: bool,
+    pub contacts: Vec<Contact>,
+}
+
+// NOTE: requires `inp` to already have the version 3 header removed
+pub fn parse_bootstrap(inp: &[u8]) -> Result<Vec<Contact>, Box<dyn Error>> {
     let mut rem = inp;
 
     if rem.len() < 4 {
@@ -50,6 +68,9 @@ pub fn parse_version_1(inp: &[u8]) -> Result<Vec<Contact>, Box<dyn Error>> {
             udp_port,
             tcp_port,
             contact_version,
+            by_type: None,
+            kad_udp_key: None,
+            verified: None,
         })
     }
 
@@ -66,8 +87,8 @@ pub fn parse(inp: &[u8]) -> Result<Vec<Contact>, Box<dyn Error>> {
     let count = u32::from_le_bytes(rem[..4].try_into().unwrap()) as usize;
     rem = &rem[4..];
 
-    let version = if count != 0 {
-        0
+    let (version, count) = if count != 0 {
+        (0, count)
     } else {
         if rem.len() < 4 {
             Err(format!("no version, have {} bytes", rem.len()))?;
@@ -75,12 +96,42 @@ pub fn parse(inp: &[u8]) -> Result<Vec<Contact>, Box<dyn Error>> {
 
         let version = u32::from_le_bytes(rem[..4].try_into().unwrap());
         rem = &rem[4..];
-        version
+
+        if version == 3 {
+            let bootstrap_edition = u32::from_le_bytes(rem[..4].try_into().unwrap());
+            rem = &rem[4..];
+
+            if bootstrap_edition == 1 {
+                // bootstrap node parsing?
+                todo!();
+            }
+        }
+
+        let count = u32::from_le_bytes(rem[..4].try_into().unwrap()) as usize;
+        rem = &rem[4..];
+        (version, count)
     };
 
     if version > 3 {
         Err(format!("unknown version {}", version))?;
     }
 
-    todo!()
+    let mut r = Vec::with_capacity(count);
+    for _ in 0..count {
+        let n = 25 + if version >= 2 { 1 + 4 + 4 } else { 0 };
+        if rem.len() < n {
+            Err(format!("not enough bytes, need {}, have {}, idx: {} of {}",
+                n, rem.len(), r.len(), count))?;
+        }
+
+
+        let c = todo!();
+        r.push(c);
+    }
+
+    if rem.len() != 0 {
+        Err(format!("spare bytes: {}", rem.len()))?;
+    }
+
+    Ok(r)
 }
