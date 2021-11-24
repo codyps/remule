3c5b6ca8c0b582a4abef834dcf6d083e166b36e5
Cody P Schafer <dev@codyps.com>
2021-11-23 20:53:00 -0500

    flake?

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    crate2nix = {
+      url = "github:kolloch/crate2nix";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crate2nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        crateName = "remule";
+
+        inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+          generatedCargoNix;
+
+        project = import (generatedCargoNix {
+          name = crateName;
+          src = ./.;
+        }) {
+          inherit pkgs;
+          defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+            buildInputs = 
+              lib.optionals pkgs.stdenv.isDarwin
+                [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
+          };
+        };
+
+      in {
+        packages.${crateName} = project.rootCrate.build;
+
+        defaultPackage = self.packages.${system}.${crateName};
+
+        devShell = pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system};
+          buildInputs = [ pkgs.cargo pkgs.rust-analyzer pkgs.clippy ];
+        };
+      });
+}
